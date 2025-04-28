31291c07235bbede36b53ae43e75f8ca3853fcc0
Caio Guerra <caio.guerra105@gmail.com>
2025-04-28 15:04:29 -0300

    Implementando as resticoes de movimento

--- Xadrez Console/Program.cs
+++ Xadrez Console/Program.cs
@@ -7,18 +7,22 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                PartidaDeXadrez partida = new PartidaDeXadrez();
+            PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                while (!partida.terminada)
+            while (!partida.terminada)
+            {
+                try
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadez().toPosicao();
+                    partida.validarPosicaoOrigem(origem);
 
                     bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
@@ -28,17 +32,16 @@ namespace Xadrez_Console
                     Console.WriteLine();
                     Console.Write("Destinho: ");
                     Posicao destino = Tela.lerPosicaoXadez().toPosicao();
+                    partida.validarPosicaoDeDestino(origem, destino);
 
-                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
                 }
-
+                catch (TabuleiroException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
             }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadLine();
         }
     }
 }
